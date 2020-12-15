57b7ca07b7cd4fd7c7c7e9291c59a605b2ab019c
fazttech <fazt@faztweb.com>
2020-12-15 12:54:03 -0500

    images

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import image from './assets/image.png'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: "#888", fontSize: 18 }}>Hello world</Text>
+      <Image source={{uri: "https://picsum.photos/200/200"}} style={styles.image} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100
+  }
 });
+
+export default App;
