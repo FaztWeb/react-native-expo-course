7748d38422021caf9b4a0b4b86d16b698d12d254
fazttech <fazt@faztweb.com>
2020-12-16 10:36:30 -0500

    build web added

--- App.js
+++ App.js
@@ -1,12 +1,86 @@
-import React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
-import image from './assets/image.png'
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+import uploadAnonymousFilesAsync from "anonymous-files";
 
 const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to camara roll is required");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    // console.log(pickerResult)
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    if (Platform.OS === "web") {
+      let remoteUri = await uploadAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri });
+    }
+  };
+
+  let openShareDialog = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      // alert("Sharing, is not available on your platform");
+      alert(
+        `The image share is available for sharing at: ${selectedImage.remoteUri}`
+      );
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
+  // if (selectedImage !== null) {
+  //   return (
+  //     <View styles={styles.container}>
+  //       <Image
+  //         source={{ uri: selectedImage.localUri }}
+  //         style={styles.thumbnail}
+  //       />
+  //     </View>
+  //   );
+  // }
+
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#888", fontSize: 18 }}>Hello world</Text>
-      <Image source={{uri: "https://picsum.photos/200/200"}} style={styles.image} />
+      <Text style={{ color: "#eee", fontSize: 20, marginBottom: 10 }}>Change this image</Text>
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image
+          source={{
+            uri:
+              selectedImage !== null
+                ? selectedImage.localUri
+                : "https://picsum.photos/200/200",
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+      {selectedImage ? (
+        <TouchableOpacity onPress={openShareDialog} style={styles.button}>
+          <Text style={styles.buttonText}>Share this Image</Text>
+        </TouchableOpacity>
+      ) : (
+        <View></View>
+      )}
     </View>
   );
 };
@@ -14,15 +88,23 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#292929",
     alignItems: "center",
     justifyContent: "center",
   },
   image: {
-    height: 200,
+    height: 100,
+    width: 100,
+    resizeMode: "contain",
+    // borderRadius: 100,
+  },
+  button: { backgroundColor: "#BB86FC", padding: 7, marginTop: 10 },
+  buttonText: { fontSize: 20, color: "#fff" },
+  thumbnail: {
     width: 200,
-    borderRadius: 100
-  }
+    height: 200,
+    resizeMode: "contain",
+  },
 });
 
 export default App;
---
+++ README.md
@@ -0,0 +1,11 @@
+# React Native Expo Course
+
+## Build
+
+web
+
+```
+expo build:android
+expo build:ios
+expo build:web
+```
